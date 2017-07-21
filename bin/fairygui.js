8ea6cb78f4d1c3ed991915e970897b770b908f10
ytom <gzytom@139.com>
2017-07-21 17:56:07 +0800

    typing bug

--- bin/fairygui.js
+++ bin/fairygui.js
@@ -7445,7 +7445,7 @@
 					if(ii<this._pageIds.length)
 						taction.toPage=[this._pageIds[ii]];
 					str=str.substring(0,k);
-					if(str=="*"){
+					if(str !="*"){
 						ii=parseInt(str);
 						if(ii<this._pageIds.length)
 							taction.fromPage=[this._pageIds[ii]];
