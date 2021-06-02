a38d3602c8b0e2acb369d8c8945e9bfe15d9d7b4
guzhu@fairygui.com <guzhu@fairygui.com>
2021-06-02 23:54:48 +0800

    fix a bug in GTweener

--- source/bin/fairygui.js
+++ source/bin/fairygui.js
@@ -16845,7 +16845,7 @@
             return this;
         }
         _toColor(start, end, duration) {
-            this._valueSize = 4;
+            this._valueSize = 5;
             this._startValue.color = start;
             this._endValue.color = end;
             this._value.color = start;
@@ -16853,7 +16853,7 @@
             return this;
         }
         _shake(startX, startY, amplitude, duration) {
-            this._valueSize = 5;
+            this._valueSize = 6;
             this._startValue.x = startX;
             this._startValue.y = startY;
             this._startValue.w = amplitude;
@@ -16948,7 +16948,7 @@
             this._normalizedTime = fgui.evaluateEase(this._easeType, reversed ? (this._duration - tt) : tt, this._duration, this._easeOvershootOrAmplitude, this._easePeriod);
             this._value.setZero();
             this._deltaValue.setZero();
-            if (this._valueSize == 5) {
+            if (this._valueSize == 6) {
                 if (this._ended == 0) {
                     var r = this._startValue.w * (1 - this._normalizedTime);
                     var rx = r * (Math.random() > 0.5 ? 1 : -1);
